d21cfd98325a98d20fa59b7e31b14c2183e70e66
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2022-09-07 20:47:12 +0600

    problem 7 solved

---
+++ problem-7/main.c
@@ -0,0 +1,20 @@
+// Write a C program to determine whether a number is negative or not
+
+#include <stdio.h>
+
+int main()
+{
+    float num;
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    if (num < 0)
+    {
+        printf("The number is negative");
+    }
+    else
+    {
+        printf("The number is not negative");
+    }
+}
